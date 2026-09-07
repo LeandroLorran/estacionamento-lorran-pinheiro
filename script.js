dd1caccf4719a7e93c0dc66637279d10c6571c53
leandro <leandrob2612@gmail.com>
2026-09-07 13:22:03 -0300

    pagina cadastro

--- script.js
+++ script.js
@@ -10,7 +10,26 @@ function entrar() {
   }
 }
 
-function sair(){
+function sair() {
   window.location.href = "../index.html";
+}
 
+function buscaCnhOuplaca() {
+  const valorSelect = document.getElementById("busca").value;
+  const inputdaConsulta = document.getElementById("consultaInput").value;
+
+  const placaCadastrada = "ABC1234";
+  const cnh = "00000000000";
+
+  if (inputdaConsulta === cnh && valorSelect === "1") {
+    window.location.href = "consulta.html";
+    return;
+  }
+  if (inputdaConsulta === placaCadastrada && valorSelect === "2") {
+    window.location.href = "consulta.html";
+  } else if (valorSelect === "1") {
+    alert("CNH não consta no sistema");
+  } else if (valorSelect === "2") {
+    alert("Placa não consta no sistema");
+  }
 }
