669523d402cc3e989bf8007d6b70b74c374b281b
leandro <leandrob2612@gmail.com>
2026-09-06 21:48:18 -0300

    logica simples login

---
+++ script.js
@@ -0,0 +1,11 @@
+function entrar() {
+  const usuario = document.getElementById("usuario").value;
+  const senha = document.getElementById("senha").value;
+
+  if (usuario === "admin" && senha === "123") {
+    window.location.href = "pages/cadastro.html";
+  } else {
+    document.getElementById("mensagem").textContent =
+      "Usuário ou senha incorretos!";
+  }
+}
