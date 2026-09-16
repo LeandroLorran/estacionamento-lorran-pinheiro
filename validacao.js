d887f80141dc8872eb9ccef6a90f418daabfb603
leandro <leandrob2612@gmail.com>
2026-09-16 13:48:29 -0300

    script validação

---
+++ validacao.js
@@ -0,0 +1,11 @@
+const nome = document.querySelector("#name");
+const cnh = document.querySelector("#cnh");
+const form = document.querySelector("form");
+
+form.addEventListener('submit', (e) => {
+
+  if(!nome.value || !nome.value){
+
+  }
+  e.preventDefault()
+})
