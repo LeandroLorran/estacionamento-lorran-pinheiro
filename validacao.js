90fe0266ef7b88561331a1153c3e87d451e10ca1
leandro <leandrob2612@gmail.com>
2026-09-17 13:29:58 -0300

    script

--- validacao.js
+++ validacao.js
@@ -1,11 +1,22 @@
-const nome = document.querySelector("#name");
+const nome = document.querySelector("#nome");
 const cnh = document.querySelector("#cnh");
-const form = document.querySelector("form");
+const form = document.querySelector("#form");
 
-form.addEventListener('submit', (e) => {
 
-  if(!nome.value || !nome.value){
 
+form.addEventListener("submit", (evento) => {
+  let enviarForm = true;
+
+
+  if (cnh.value.length !== 11) {
+    console.log("A CNH está incorreta");
+
+    enviarForm = false;
+    cnh.style.borderColor= "red";
+    
   }
-  e.preventDefault()
-})
+
+  if (!enviarForm) {
+    evento.preventDefault();
+  }
+});
